89c33e0926d7477604836f0c73fe23ac8349f29f
lixxxiang <13051744716@163.com>
2017-03-31 11:33:19 +0800

    add Distance计算

--- app/src/main/java/com/cgwx/yyfwptz/lixiang/leifeng0_2/view/activity/MainActivity.java
+++ app/src/main/java/com/cgwx/yyfwptz/lixiang/leifeng0_2/view/activity/MainActivity.java
@@ -4,12 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import com.cgwx.yyfwptz.lixiang.leifeng0_2.presenters.mainActivitypresenter.MainActivityPresenter;
+
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import com.cgwx.yyfwptz.lixiang.leifeng0_2.R;
 import com.cgwx.yyfwptz.lixiang.leifeng0_2.view.BaseViewInterface;
+
+import static com.cgwx.yyfwptz.lixiang.leifeng0_2.utils.Distance.getDistance;
 
 
 public class MainActivity extends BaseActivity<MainActivityPresenter, MainActivity> implements RadioGroup.OnCheckedChangeListener, BaseViewInterface{
@@ -35,9 +39,10 @@ public class MainActivity extends BaseActivity<MainActivityPresenter, MainActivi
         record.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.e("getDidddd", String.valueOf(getDistance(44,125.41,44.03,125.44)));
 //                presenter.performOnClick();//test
-                Intent intent = new Intent(MainActivity.this, getLocationDemo.class);
-                MainActivity.this.startActivity(intent);
+//                Intent intent = new Intent(MainActivity.this, getLocationDemo.class);
+//                MainActivity.this.startActivity(intent);
             }
         });
 
